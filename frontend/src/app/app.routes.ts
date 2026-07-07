7c15e17fe15d6661a7dab9e713aebc2003e8e230
sahilgiglogistics <sharma.sahil@giglogistics.com>
2026-07-07 19:19:42 +0530

    feat(frontend): Home page layout added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: Home }
 ];
